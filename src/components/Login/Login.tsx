851f178f5966f4f232effeda6320458ab5f237ca
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2024-01-16 22:46:26 -0300

    styled: login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -5,6 +5,7 @@ import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import React from "react";
 import { UserContext } from "../../UserContext";
+import styles from "./Login.module.css";
 
 const Login = () => {
     const { login }: any = React.useContext(UserContext);
@@ -12,14 +13,17 @@ const Login = () => {
     if (login === true) return <Navigate to="/conta" />
 
     return (
-        <div>
-            <Routes>
-                <Route path="/" element={<LoginForm />} />
-                <Route path="criar" element={<LoginCreate />} />
-                <Route path="perdeu" element={<LoginPasswordLost />} />
-                <Route path="resetar" element={<LoginPasswordReset />} />
-            </Routes>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path="/" element={<LoginForm />} />
+                    <Route path="criar" element={<LoginCreate />} />
+                    <Route path="perdeu" element={<LoginPasswordLost />} />
+                    <Route path="resetar" element={<LoginPasswordReset />} />
+                </Routes>
+            </div>
+
+        </section>
     );
 }
 
