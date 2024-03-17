3c1e6b1b0d31f881a3545cbc8efdedc3b00d90c3
Isaque Chaves <isaque.ramosch@gmail.com>
2024-03-17 16:09:18 -0300

    fix: fix NavLink bug

--- src/components/User/UserHeaderNav/UserHeaderNav.tsx
+++ src/components/User/UserHeaderNav/UserHeaderNav.tsx
@@ -8,24 +8,27 @@ import Sair from "../../../Assets/sair.svg";
 import styles from "./UserHeaderNav.module.css";
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
+  const [mobile, setMobile] = React.useState<boolean>(false);
   const { userLogout }: any = React.useContext(UserContext);
 
   return (
     <nav className={styles.nav}>
-      <NavLink to="/conta" end activeClassName={styles.active}>
+      <NavLink to="/conta" end>
         <img src={MinhasFotos} alt="Minhas Fotos" />
         {mobile && "Minhas Fotos"}
       </NavLink>
-      <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+      <NavLink to="/conta/estatisticas">
         <img src={Estatisticas} alt="Estatisticas" />
         {mobile && "Estatisticas"}
       </NavLink>
-      <NavLink to="/conta/postar" activeClassName={styles.active}>
+      <NavLink to="/conta/postar">
         <img src={AdicionarFoto} alt="Adicionar Foto" />
         {mobile && "Adicionar Foto"}
       </NavLink>
-      <button onClick={userLogout}>
+      <button onClick={() => {
+  userLogout();
+  setMobile(false);
+}}>
         <img src={Sair} alt="Sair" />
         {mobile && "Sair"}
       </button>
